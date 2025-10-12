085924ea42d939c77f0e8528c69deb27ee92712b
Igor <zakatov0303@gmail.com>
2025-10-12 13:30:52 +0300

    Move utils function to separate file

--- rest/src/main/kotlin/com/zakat/classifier/ClassifierService.kt
+++ rest/src/main/kotlin/com/zakat/classifier/ClassifierService.kt
@@ -45,8 +45,7 @@ class ClassifierService {
             throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Answer is not tensor")
 
         val value = output.value as Array<FloatArray>
-        val logits = value[0]
-        val probs = softmax(logits)
+        val probs = softmax(value[0])
         val predictedClass = argmax(probs)
 
         return PredictionResult(
@@ -54,75 +53,5 @@ class ClassifierService {
             predictedClass = predictedClass,
             predictedClassTitle = predictClasses[predictedClass],
         )
-    }
-
-    private fun argmax(nums: List<Float>): Int {
-        var argMax = 0
-        for (i in nums.indices) {
-            if (nums[i] > nums[argMax]) {
-                argMax = i
-            }
-        }
-
-        return argMax
-    }
-
-    private fun softmax(logits: FloatArray): List<Float> {
-        val expSum = logits
-            .map { logit -> exp(logit) }
-            .sum()
-
-        return logits.map { logit -> exp(logit) / expSum }
-    }
-
-    private fun convertImageToTensor(env: OrtEnvironment, image: MultipartFile): OnnxTensor =
-        image.inputStream.use { stream ->
-            val bufferedImage = ImageIO.read(stream)
-            val data = preprocessImage(bufferedImage)
-            val buffer = FloatBuffer.wrap(data)
-
-            val shape = longArrayOf(1, 3, 200, 200)
-            OnnxTensor.createTensor(env, buffer, shape)
-        }
-
-    private fun preprocessImage(image: BufferedImage): FloatArray {
-        val resizedImage = resizeImage(image, 200, 200)
-        val width = resizedImage.width
-        val height = resizedImage.height
-
-        val data = FloatArray(3 * 200 * 200)
-        val pixels = resizedImage.getRGB(0, 0, width, height, null, 0, width)
-
-        for (i in 0 until pixels.size) {
-            val pixel = pixels[i]
-            var r = ((pixel shr 16) and 0xFF) / 255.0f
-            var g = ((pixel shr 8) and 0xFF) / 255.0f
-            var b = (pixel and 0xFF) / 255.0f
-
-            r = (r - .5f) / .5f
-            g = (r - .5f) / .5f
-            b = (r - .5f) / .5f
-
-            val row = i / width
-            val col = i % width
-
-            data[row * width + col] = r
-            data[1 * height * width + row * width + col] = g
-            data[2 * height * width + row * width + col] = b
-        }
-
-        return data
-    }
-
-    private fun resizeImage(image: BufferedImage, width: Int, height: Int): BufferedImage {
-        val g = image.createGraphics()
-        g.setRenderingHints(
-            mapOf(
-                RenderingHints.KEY_INTERPOLATION to RenderingHints.VALUE_INTERPOLATION_BILINEAR,
-            )
-        )
-        g.drawImage(image, 0, 0, width, height, null)
-        g.dispose()
-        return image
     }
 }
---
+++ rest/src/main/kotlin/com/zakat/classifier/MLUtils.kt
@@ -0,0 +1,80 @@
+package com.zakat.classifier
+
+import ai.onnxruntime.OnnxTensor
+import ai.onnxruntime.OrtEnvironment
+import org.springframework.web.multipart.MultipartFile
+import java.awt.RenderingHints
+import java.awt.image.BufferedImage
+import java.nio.FloatBuffer
+import javax.imageio.ImageIO
+import kotlin.math.exp
+
+fun argmax(nums: List<Float>): Int {
+    var argMax = 0
+    for (i in nums.indices) {
+        if (nums[i] > nums[argMax]) {
+            argMax = i
+        }
+    }
+
+    return argMax
+}
+
+fun softmax(logits: FloatArray): List<Float> {
+    val expSum = logits
+        .map { logit -> exp(logit) }
+        .sum()
+
+    return logits.map { logit -> exp(logit) / expSum }
+}
+
+fun convertImageToTensor(env: OrtEnvironment, image: MultipartFile): OnnxTensor =
+    image.inputStream.use { stream ->
+        val bufferedImage = ImageIO.read(stream)
+        val data = preprocessImage(bufferedImage)
+        val buffer = FloatBuffer.wrap(data)
+
+        val shape = longArrayOf(1, 3, 200, 200)
+        OnnxTensor.createTensor(env, buffer, shape)
+    }
+
+fun preprocessImage(image: BufferedImage): FloatArray {
+    val resizedImage = resizeImage(image, 200, 200)
+    val width = resizedImage.width
+    val height = resizedImage.height
+
+    val data = FloatArray(3 * 200 * 200)
+    val pixels = resizedImage.getRGB(0, 0, width, height, null, 0, width)
+
+    for (i in 0 until pixels.size) {
+        val pixel = pixels[i]
+        var r = ((pixel shr 16) and 0xFF) / 255.0f
+        var g = ((pixel shr 8) and 0xFF) / 255.0f
+        var b = (pixel and 0xFF) / 255.0f
+
+        r = (r - .5f) / .5f
+        g = (r - .5f) / .5f
+        b = (r - .5f) / .5f
+
+        val row = i / width
+        val col = i % width
+
+        data[row * width + col] = r
+        data[1 * height * width + row * width + col] = g
+        data[2 * height * width + row * width + col] = b
+    }
+
+    return data
+}
+
+fun resizeImage(image: BufferedImage, width: Int, height: Int): BufferedImage {
+    val g = image.createGraphics()
+    g.setRenderingHints(
+        mapOf(
+            RenderingHints.KEY_INTERPOLATION to RenderingHints.VALUE_INTERPOLATION_BILINEAR,
+        )
+    )
+    g.drawImage(image, 0, 0, width, height, null)
+    g.dispose()
+    return image
+}
